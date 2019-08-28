59ee6155ec39bc0ddd6f9029b98c362a01d72c3a
Mary Mckeon <mmckeo@a1990-14280.icims.net>
2019-08-28 10:20:54 -0400

    using useEffect

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -1,29 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from './Title';
 import PhotoWall from "./PhotoWall";
 
-const posts = [{
-    id: "0",
-    description: "landscape",
-    imageLink: "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM-650-80.jpg"
-},
-{
-    id: "1",
-    description: "alien",
-    imageLink: "https://compote.slate.com/images/248266ae-bc3c-4aa2-a8d0-69a3f65d5500.jpg"
-},
-{
-    id: "2",
-    description: "vacay",
-    imageLink: "https://atlantis.nyc3.digitaloceanspaces.com/styled/8f4597e8d337d22501cede98b6b0ecb4"
-}]
-
-
-
 const Main = (props) => {
-    const [postList, setPostList] = useState(posts);
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        const data = simDBRequest();
+        setPostList(data);
+    }, []);
+
     const removePhoto = (postRemoved) => {
-        console.log(postRemoved.description);
         setPostList(postList.filter(postList => postList !== postRemoved));
     }
     return (
@@ -33,5 +20,23 @@ const Main = (props) => {
         </>
     )
 };
+
+const simDBRequest = () =>  (
+    [{
+        id: "0",
+        description: "landscape",
+        imageLink: "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM-650-80.jpg"
+    },
+    {
+        id: "1",
+        description: "alien",
+        imageLink: "https://compote.slate.com/images/248266ae-bc3c-4aa2-a8d0-69a3f65d5500.jpg"
+    },
+    {
+        id: "2",
+        description: "vacay",
+        imageLink: "https://atlantis.nyc3.digitaloceanspaces.com/styled/8f4597e8d337d22501cede98b6b0ecb4"
+    }]
+);
 
 export default Main;
--- photowall/src/Components/Photo.js
+++ photowall/src/Components/Photo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Photo = (props) => (
     //const post = this.props.post
