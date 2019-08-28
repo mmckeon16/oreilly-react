62b86b1fd4b2ca80204a52bb4517f058303274b6
Mary Mckeon <mmckeo@a1990-14280.icims.net>
2019-08-28 14:21:28 -0400

    implementing redux - have data loading through a reducer as default

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -1,65 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Title from './Title';
 import PhotoWall from "./PhotoWall";
 import AddPhoto from "./AddPhoto";
 import {Route} from "react-router-dom";
 
 const Main = (props) => {
-    const [postList, setPostList] = useState([]);
-
-    useEffect(() => {
-        const data = simDBRequest();
-        setPostList(data);
-    }, []);
-
-    //functions
-    const removePhoto = (postRemoved) => {
-        setPostList(postList.filter(postList => postList !== postRemoved));
-    }
-
-    const addPhoto = (postSubmitted) => {
-        setPostList(postList.concat([postSubmitted]));
-    }
-
     return (
         <>
-            {console.log(postList)}
+            {console.log(props.posts)}
             <Route exact path = "/" render ={() => (
                 <div>
                     <Title title={'Photowall'}/>
                     <PhotoWall 
-                        posts={postList} 
-                        onRemovePhoto = {removePhoto}
+                        posts={props.posts} 
                     />
                 </div>
             )} /> 
 
-            <Route path = "/add-photo" render = {({history}) => (
+            {/* <Route path = "/add-photo" render = {({history}) => (
                 <AddPhoto onAddPhoto={(addedPost) => {
                     addPhoto(addedPost);
                     history.push("/");
                 }} />
-            )}/>
+            )}/> */}
         </>
     )
 };
 
-const simDBRequest = () =>  (
-    [{
-        id: 0,
-        description: "landscape",
-        imageLink: "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM-650-80.jpg"
-    },
-    {
-        id: 1,
-        description: "alien",
-        imageLink: "https://compote.slate.com/images/248266ae-bc3c-4aa2-a8d0-69a3f65d5500.jpg"
-    },
-    {
-        id: 2,
-        description: "vacay",
-        imageLink: "https://atlantis.nyc3.digitaloceanspaces.com/styled/8f4597e8d337d22501cede98b6b0ecb4"
-    }]
-);
 
 export default Main;
---
+++ photowall/src/redux/reducer.js
@@ -0,0 +1,7 @@
+import theposts from '../data/posts';
+
+const postReducer = function posts(state=theposts, action) {
+    return state;
+}
+
+export default postReducer;
